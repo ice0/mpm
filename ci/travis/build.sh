5864f8aecba9e52f06c00164115d1b22f9f1b1fc
ice0 <az@localhost.localdomain>
2018-05-01 16:35:53 +0700

    fixed haxors

--- ci/travis/build.sh
+++ ci/travis/build.sh
@@ -5,7 +5,4 @@ cd ./zlib-1.2.11
 ./configure --prefix=../build
 make && make install
 cd ../
-echo "$GH_TOKEN" > ./build/key.txt
-echo "$SECURE_TEST" >> ./build/key.txt
-echo "secretvalue"
 tar czf $BUILD_FILENAME ./build
